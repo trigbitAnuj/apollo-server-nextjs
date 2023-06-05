d3f945ef2d12be85c40abd33799018d22f9ec496
Anuj kumar <trigbit.anuj@gmail.com>
2023-06-05 18:38:55 +0530

    Create apollo-provider.tsx

---
+++ src/app/lib/apollo-provider.tsx
@@ -0,0 +1,45 @@
+"use client";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  SuspenseCache,
+} from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  SSRMultipartLink,
+  NextSSRInMemoryCache,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+import { ReactNode } from "react";
+
+const makeClient = () => {
+  const httpLink = new HttpLink({
+    uri: "http://localhost:3000/api/graphql",
+  });
+
+  return new ApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({ stripDefer: true }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+};
+
+const makeSuspenseCache = () => {
+  return new SuspenseCache();
+};
+
+export const ApolloWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <ApolloNextAppProvider
+      makeClient={makeClient}
+      makeSuspenseCache={makeSuspenseCache}
+    >
+      {children}
+    </ApolloNextAppProvider>
+  );
+};
